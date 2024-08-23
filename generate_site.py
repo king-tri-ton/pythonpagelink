cd2ef765ce0b47e47141084e2d29892d2cd9cef0
king-tri-ton <mdolmatov99@gmail.com>
2024-08-23 17:41:14 +0500

    bug fix

--- generate_site.py
+++ generate_site.py
@@ -8,7 +8,7 @@ with open('config.yml', 'r') as config_file:
     config = yaml.safe_load(config_file)
 
 # Создание выходной директории
-output_dir = 'root'
+output_dir = 'output'
 os.makedirs(output_dir, exist_ok=True)
 
 # Настройка Jinja2
